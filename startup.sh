c7e425e4ee19e98de541625f98a5771f40614608
k-seta <swordh16@gmail.com>
2023-01-23 22:17:04 +0900

    Revert "mongodbをdocker compose に追加"
    This reverts commit 91b9993b89928ec49741b277c4448e519ea146ff.

--- startup.sh
+++ startup.sh
@@ -15,4 +15,4 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
-    docker/compose:1.29.2 --profile production up
+    docker/compose:1.29.2 up
